6d6626ebaa0013601303c1e0331e3fef46889c08
JosefVazanic <josef.vazanic@gmx.at>
2013-01-07 21:22:36 +0100

    Details angepasst

--- src/at/edu/uas/fmapp/ResultObjectsInformation.java
+++ src/at/edu/uas/fmapp/ResultObjectsInformation.java
@@ -2,14 +2,27 @@ package at.edu.uas.fmapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import at.edu.uas.fmapp.utils.FmApp;
 
 public class ResultObjectsInformation extends Activity {
 
+	private FmApp appState;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_result_objects_information);
+		
+		appState = (FmApp) getApplicationContext();
+		Button loginName = (Button) findViewById(R.id.loginName);
+		loginName.setText(appState.getLoggedInPerson());
+
+		// setTitle(appState.getSelectedItem().getName() + " - "
+		// + appState.getSelectedItem().getAddress());
 	}
 
 	@Override
@@ -20,4 +33,16 @@ public class ResultObjectsInformation extends Activity {
 		return true;
 	}
 
+	public void logout(View v) {
+		startActivity(new Intent(this, WelcomeActivity.class));
+		appState.logout();
+	}
+
+	public void loadHome(View v) {
+		startActivity(new Intent(this, HomeActivity.class));
+	}
+
+	public void loadObject(View v) {
+		startActivity(new Intent(this, ObjectDetail.class));
+	}
 }
--- src/at/edu/uas/fmapp/ResultObjectsWay.java
+++ src/at/edu/uas/fmapp/ResultObjectsWay.java
@@ -2,14 +2,27 @@ package at.edu.uas.fmapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import at.edu.uas.fmapp.utils.FmApp;
 
 public class ResultObjectsWay extends Activity {
+
+	private FmApp appState;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_result_objects_way);
+
+		appState = (FmApp) getApplicationContext();
+		Button loginName = (Button) findViewById(R.id.loginName);
+		loginName.setText(appState.getLoggedInPerson());
+
+		// setTitle(appState.getSelectedItem().getName() + " - "
+		// + appState.getSelectedItem().getAddress());
 	}
 
 	@Override
@@ -19,4 +32,16 @@ public class ResultObjectsWay extends Activity {
 		return true;
 	}
 
+	public void logout(View v) {
+		startActivity(new Intent(this, WelcomeActivity.class));
+		appState.logout();
+	}
+
+	public void loadHome(View v) {
+		startActivity(new Intent(this, HomeActivity.class));
+	}
+
+	public void loadObject(View v) {
+		startActivity(new Intent(this, ObjectDetail.class));
+	}
 }
