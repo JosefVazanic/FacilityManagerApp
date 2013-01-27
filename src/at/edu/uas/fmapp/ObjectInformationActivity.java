ea2a8bc98e73afad25b2dba06330ec69a777c26b
JosefVazanic <josef.vazanic@gmx.at>
2013-01-27 15:14:56 +0100

    Information Activity started

--- src/at/edu/uas/fmapp/ObjectInformationActivity.java
+++ src/at/edu/uas/fmapp/ObjectInformationActivity.java
@@ -1,12 +1,25 @@
 package at.edu.uas.fmapp;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import at.edu.uas.fmapp.entity.Person;
+import at.edu.uas.fmapp.entity.WorkObject;
 
 public class ObjectInformationActivity extends LoggedInBaseActivity {
+	
+	private WorkObject currentWorkObject;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_object_information);
+		
+		currentWorkObject = appState.getSelectedItem();
+		Person contactPerson = currentWorkObject.getContactPerson();
+		
+		// ContactPersonName
+		String strContactPersonName = contactPerson.getFirstName() + " " + contactPerson.getLastName();
+		TextView txtContactPersonName = (TextView) findViewById(R.id.textViewContactPersonName);
+		txtContactPersonName.setText(strContactPersonName);
 	}
 }
