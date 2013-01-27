44c05997d4cf0fdad689be4d79b1a9357ad62622
Reinhard Freiler <r.freiler@gmx.at>
2013-01-27 20:50:14 +0100

    test calls for work item server methods

--- src/at/edu/uas/fmapp/HomeActivity.java
+++ src/at/edu/uas/fmapp/HomeActivity.java
@@ -1,12 +1,15 @@
 package at.edu.uas.fmapp;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import at.edu.uas.fmapp.entity.TaskAssignment;
+import at.edu.uas.fmapp.entity.WorkItem;
 import at.edu.uas.fmapp.entity.WorkObject;
 import at.edu.uas.fmapp.entity.Worker;
 import at.edu.uas.fmapp.server.FmServiceExecutionListener;
@@ -48,6 +51,53 @@ public class HomeActivity extends LoggedInBaseActivity {
 
 		appState.getProxy().getWorkObjectList(
 				appState.getLoggedInPerson().getId(), executionListener);
+		
+		//TODO test code. remove it.
+		FmServiceExecutionListener<List<WorkItem>> executionListener2 = new FmServiceExecutionListener<List<WorkItem>>() {
+
+			@Override
+			public void onPostExecute(List<WorkItem> result) {
+			
+				WorkItem workItemToUpdate = result.get(0);
+				workItemToUpdate.setStatus("Done");
+				
+				FmServiceExecutionListener<WorkItem> executionListener2 = new FmServiceExecutionListener<WorkItem>() {
+
+					@Override
+					public void onPostExecute(WorkItem result) {
+						
+						WorkItem workItemToInsert = new WorkItem();
+						workItemToInsert.setStatus("Done");
+						workItemToInsert.setTaskAssignmentId(3L);
+						workItemToInsert.setDate(new Date(System.currentTimeMillis()));
+						
+						FmServiceExecutionListener<WorkItem> executionListener3 = new FmServiceExecutionListener<WorkItem>() {
+
+							@Override
+							public void onPostExecute(WorkItem result) {
+								
+								System.out.println("");
+							}
+						};
+
+						HomeActivity.this.appState.getProxy().insertOrUpdateWorkItem(workItemToInsert, executionListener3);
+						
+					}
+				};
+			
+				HomeActivity.this.appState.getProxy().insertOrUpdateWorkItem(workItemToUpdate, executionListener2);
+				
+			}
+		};
+
+		List<TaskAssignment> taList = new ArrayList<TaskAssignment>();
+		taList.add(new TaskAssignment(1L));
+		taList.add(new TaskAssignment(2L));
+	
+		appState.getProxy().getWorkItemListForAssignments(taList, executionListener2);
+		
+	
+		
 	}
 
 	public void onAllObjectsClick(View v) {
